09a298946c80ac82cc58fdc6677e972671ebbfce
dev <gmk.hussain@gmail.com>
2022-01-27 21:26:58 +0500

    initial

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+
+    <TableRow msg="table" />
+
+    <hr/>
+
+    <TableCol />
+
+  </div>
+</template>
+
+<script>
+import TableRow from './components/TableRow.vue'
+import TableCol from './components/TableCol.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TableRow,
+    TableCol
+  }
+}
+</script>
---
+++ src/components/TableRow.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello">
+    
+    <p>{{ msg }}</p>
+    <table class="table" border="1" id="tt">
+      <draggable v-model="myArray" group="people" @start="drag=true" @end="drag=false">
+          <tr v-for="element in myArray" :key="element.id">
+            <td>{{element.id}}</td>
+            <td>{{element.name}}</td>
+          </tr>
+      </draggable>
+    </table>
+  </div>
+</template>
+
+<script>
+import draggable from 'vuedraggable'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      myArray: [
+        {
+          id: 1001,
+          name: "A for Apple"
+        },
+        {
+          id: 1002,
+          name: "B for Berry"
+        },
+        {
+          id: 1003,
+          name: "C for Cherry"
+        },
+        {
+          id: 1004,
+          name: "D for Duck"
+        }
+      ]
+    }
+  },
+  mounted() {
+    
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
